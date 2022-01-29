8ca5cf3e0bacb7c3ee8de3b7115304fb0ac30d83
Marko Chylik <60507938+macricek@users.noreply.github.com>
2022-01-29 22:17:41 +0100

    FastAI ready for Carla integration (#5)
    * FastAI line detector class implementation
    
    * FastAI line detector class r2i

---
+++ fastAI/FALineDetector.py
@@ -0,0 +1,83 @@
+import copy
+import os
+import time
+
+from fastai.vision.all import *
+import torch
+import cv2
+import numpy as np
+from fastAI import *
+
+
+class FALineDetector:
+    torchModel: str
+    fastAiModel: str
+    image: ndarray
+    left: ndarray
+    right: ndarray
+    mask: ndarray
+
+    def __init__(self, aug=True, learner=False):
+        self.importModels(aug)
+        self.useLearner = learner
+        self.device = torch.cuda.device(0)
+        self.learner = load_learner(self.fastAiModel)
+        self.model = self.learner.model.cuda()
+        self.time = time.time()
+        self.initLearners()
+
+    def importModels(self, aug):
+        self.torchModel = os.path.abspath('fastai_model.pth')
+        self.fastAiModel = os.path.abspath('seg.pkl')
+        if aug:
+            self.torchModel = self.torchModel.replace("model", "model_aug")
+            self.fastAiModel = self.fastAiModel.replace("seg", "seg_aug")
+
+    def predictByLearner(self):
+        self.mask = np.array(self.learner.predict(self.image)[0])
+
+    def predict(self):
+        with torch.no_grad():
+            image_tensor = self.image.transpose(2, 0, 1).astype('float32') / 255
+            x_tensor = torch.from_numpy(image_tensor).to("cuda").unsqueeze(0)
+            _, self.left, self.right = F.softmax(self.model.forward(x_tensor), dim=1).cpu().numpy()[0]
+
+    def visualize(self):
+        actImage = self.image
+        if self.useLearner:
+            actImage[self.model > 0.3, :] = [0, 255, 0]  # use green separators
+        else:
+            actImage[self.left > 0.3, :] = [0, 0, 255]  # blue for left
+            actImage[self.right > 0.3, :] = [255, 0, 0]  # red for right
+        cv2.imshow("Visualization of LineDetection", actImage)
+        cv2.waitKey(1)
+
+    def loadImage(self, path=None, numpyArr=None):
+        if path is not None:
+            self.image = cv2.imread(path)
+            self.image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
+        else:
+            self.image = numpyArr
+
+    # First detection takes 2-3s so make "fake" on beginning
+    def initLearners(self):
+        self.loadImage(path="im.png")
+        self.predictByLearner() if self.useLearner else self.predict()
+
+    def sinceLast(self):
+        since = time.time() - self.time
+        self.time = time.time()
+        return since
+
+
+if __name__ == '__main__':
+    fald = FALineDetector(aug=True, learner=False)
+    d = os.path.join("../Kaggle/val/")
+    for file in os.listdir("../Kaggle/val"):
+        fileP = os.path.join(d + file)
+        print(fileP)
+        print(fald.sinceLast())
+        fald.loadImage(path=fileP)
+        fald.predict()
+        fald.visualize()
+        #time.sleep(0.5)
--- fastAI/fastAItest.py
+++ fastAI/fastAItest.py
@@ -6,14 +6,16 @@ import cv2
 import numpy as np
 import albumentations as albu
 import os
+import copy
 import torch
 from fastseg import MobileV3Small
 from fastAI import get_image_array_from_fn, label_func
 
 USE_AUG = True
-pathToLearner = "fastAI\\seg.pkl"
+pathToLearner = "seg.pkl"
 if USE_AUG:
     pathToLearner.replace("seg", "seg_aug")
+
 
 def predict(model, img_array):
     print("Start")
@@ -30,16 +32,28 @@ def predict(model, img_array):
 
 if __name__ == '__main__':
     torch.cuda.device(0)
-    DATA_DIR = "Kaggle/"
+    DATA_DIR = "../Kaggle/"
     x_train_dir = os.path.join(DATA_DIR, 'train')
     y_train_dir = os.path.join(DATA_DIR, 'train_label')
 
     x_valid_dir = os.path.join(DATA_DIR, 'val')
     y_valid_dir = os.path.join(DATA_DIR, 'val_label')
-
     img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     learn = load_learner(pathToLearner)
+    back, left, right = predict(learn.model.cuda(), img)[0]
+
+    def ld_detection_overlay(image, left_mask, right_mask):
+        res = copy.copy(image)
+        res[left_mask > 0.3, :] = [0, 0, 255]
+        res[right_mask > 0.3, :] = [255, 0, 0]
+        return res
+
+
+    plt.imshow(ld_detection_overlay(img, left, right))
+    plt.show()
+
+
     for i in range(0, 10):
         img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -51,10 +65,14 @@ if __name__ == '__main__':
         start = time.time()
         img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        x = learn.predict(img)
         ima = np.array(learn.predict(img)[0])
         end = time.time()
         s = end - start
         print(s)
     plt.imshow(ima)
     plt.show()
+
+
+
 
