f46a6d5d1989d84c4ccec0e8c34dd0c150d36f86
Marko Chylik <marko.chylik@gmail.com>
2022-01-29 12:52:21 +0100

    FastAI line detector class implementation

--- fastAItest.py
+++ fastAItest.py
@@ -6,6 +6,7 @@ import cv2
 import numpy as np
 import albumentations as albu
 import os
+import copy
 import torch
 from fastseg import MobileV3Small
 from fastAI import get_image_array_from_fn, label_func
@@ -36,10 +37,22 @@ if __name__ == '__main__':
 
     x_valid_dir = os.path.join(DATA_DIR, 'val')
     y_valid_dir = os.path.join(DATA_DIR, 'val_label')
-
     img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     learn = load_learner(pathToLearner)
+    back, left, right = predict(learn.model.cuda(), img)[0]
+
+    def ld_detection_overlay(image, left_mask, right_mask):
+        res = copy.copy(image)
+        res[left_mask > 0.3, :] = [0, 0, 255]
+        res[right_mask > 0.3, :] = [255, 0, 0]
+        return res
+
+
+    plt.imshow(ld_detection_overlay(img, left, right))
+    plt.show()
+
+
     for i in range(0, 10):
         img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -51,10 +64,14 @@ if __name__ == '__main__':
         start = time.time()
         img = cv2.imread(str(get_image_files(x_valid_dir)[3]))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        x = learn.predict(img)
         ima = np.array(learn.predict(img)[0])
         end = time.time()
         s = end - start
         print(s)
     plt.imshow(ima)
     plt.show()
+
+
+
 
