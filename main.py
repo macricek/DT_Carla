b7d4be4162a864cf03fa31db5099ae7efa07456c
Marko Chylik <marko.chylik@gmail.com>
2022-03-11 20:04:34 +0100

    Ok, let's merge this, we need new inputs
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ class Main(QCoreApplication):
         super(Main, self).__init__([])
         self.time = time.time()
         self.carlaEnvironment = CarlaEnvironment(self,  False)
-        self.carlaEnvironment.train()
-        #self.carlaEnvironment.testRide()
+        #self.carlaEnvironment.train()
+        self.carlaEnvironment.testRide(12)
 
     def terminate(self):
         print("Terminating MAIN!")
