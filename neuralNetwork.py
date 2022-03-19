Radar normalization fix
Signed-off-by: Marko Chylik <marko.chylik@gmail.com>

neuralNetwork.py
@@ -122,4 +122,4 @@ class NeuralNetwork:
 
     @staticmethod
     def normalizeRadarInputs(radar: np.ndarray) -> np.ndarray:
-        return 1 - radar / 50
+        return 1 - (radar / 50)