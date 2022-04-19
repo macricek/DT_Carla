0b435bbedb090f7f7d71d0147c912e8235be604d
Marko Chylik <60507938+macricek@users.noreply.github.com>
2022-04-20 00:06:33 +0200

    Archive results (#23)
    * Archive some results, prepare new ones
    
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>
    
    * Prepare script for plotting comparasion
    
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>
    
    * Another script
    
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>
    
    * RADAR
    
    Signed-off-by: macricek <marko.chylik@gmail.com>
    
    * Newones
    
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>

--- main.py
+++ main.py
@@ -11,20 +11,18 @@ from fastAI.CameraGeometry import CameraGeometry
 
 class Results(enum.IntEnum):
     none = -1
-    all_possible_inputs = 2
-    Metrics_Binary_Navigation = 5
-    Metrics_Binary_Navigation_2 = 6
-    Lines_Metrics_Binary_Navigation = 22
-    Only_Lines = 61
-    Lines_Radar_Agent = 103
-    Metrics_Binary = 300
-    All_Except_Navigation = 301
+    All = 2
+    Navigation = 5
+    Lines_Navigation = 22
+    Radar = 204
+    Lines = 300
+    Lines_Radar = 301
 
     def __str__(self):
         return self.value
 
     def title(self):
-        return self.name.replace("_", " ")
+        return self.name.replace("_", ", ")
 
 
 class Mode(enum.IntEnum):
@@ -87,8 +85,8 @@ class Main(QCoreApplication):
 
 
 if __name__ == '__main__':
-    data = Results.Lines_Metrics_Binary_Navigation
-    mode = Mode.showTrainingAdvanced
+    data = Results.Radar
+    mode = Mode.runTestDefault
     mainApp = Main(data, mode)
     code = mainApp.exec()
     sys.exit(code)
