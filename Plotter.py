2f3006d8f132b75a2a9627785c6b812b81eabeb0
Marko Chylik <marko.chylik@gmail.com>
2022-04-21 20:10:42 +0200

    Hopefully last changes to PLOTTER
    Signed-off-by: Marko Chylik <marko.chylik@gmail.com>

--- Plotter.py
+++ Plotter.py
@@ -59,30 +59,32 @@ def plotPath(numRevision, type=0):
     X = f'results/{numRevision}/X{type}.csv'
     Y = f'results/{numRevision}/Y{type}.csv'
     xNp = np.loadtxt(X, delimiter=',')
-    xNp = xNp[np.where(xNp != 0)]
     yNp = np.loadtxt(Y, delimiter=',')
-    yNp = yNp[np.where(yNp != 0)]
 
     xStart = np.array([xNp[0, 0]])
     yStart = np.array([yNp[0, 0]])
-    if type == 0:
-        xEnd = np.array([-490])
-        yEnd = np.array([174])
-    else:
-        xEnd = np.array([211.2])
-        yEnd = np.array([-392.1])
+    xEnd, yEnd = getEnding(type)
     ran = xNp.shape[0]
 
     plt.figure()
     style = ['k', 'r--', 'b--', 'g--']
-    labels = ['Real path', 'LeftLine', 'RightLane', 'Optimal path']
+    labels = ['Real path', 'Right lane', 'Left lane', 'Optimal path']
     plt.title(f"Recorded path of vehicle [{numRevision.title()}]")
     plt.xlabel("x")
     plt.ylabel("y")
     plt.plot(xStart, yStart, 'c*', label="Start")
     plt.plot(xEnd, yEnd, 'y*', label="End")
-    for i in range(ran):
-        plt.plot(xNp[i, :], yNp[i, :], style[i], label=labels[i])
+    if type < 2:
+        for i in range(ran):
+            plt.plot(xNp[i, :], yNp[i, :], style[i], label=labels[i])
+    else:
+        plt.plot(xNp[0, :], yNp[0, :], style[0], label=labels[0])
+        plt.plot(xNp[3, :], yNp[3, :], style[3], label=labels[3])
+        plt.plot(xNp[1, np.where(xNp[1] != 0)], yNp[1, np.where(yNp[1] != 0)], 'r.')
+        plt.plot(xNp[2, np.where(xNp[2] != 0)], yNp[2, np.where(yNp[2] != 0)], 'b.')
+        plt.plot(xNp[1, 0], yNp[1, 0], 'r.', label=labels[1])
+        plt.plot(xNp[2, 0], yNp[2, 0], 'b.', label=labels[2])
+
     plt.legend()
     plt.savefig(f'results/{numRevision}/path{type}.png')
 
@@ -125,18 +127,43 @@ def plotPathAllInOne(type):
         yNp = np.loadtxt(Y, delimiter=',')
 
         if mem.value == 2:
-            labels = [mem.title(), 'LeftLine', 'RightLane']
+            labels = [mem.title(), 'RightLane', 'LeftLine']
             for i in range(3):
-                if i > 0:
-                    plt.plot(xNp[i, np.where(xNp[i] != 0)], yNp[i, np.where(yNp[i] != 0)], 'r*', label=labels[i])
+                if i == 1:
+                    if type == 2:
+                        plt.plot(xNp[i, np.where(xNp[i] != 0)], yNp[i, np.where(yNp[i] != 0)], 'r.')
+                    else:
+                        plt.plot(xNp[i, :], yNp[i, :], 'r--', linewidth=1, label=labels[i])
+                elif i == 2:
+                    if type == 2:
+                        plt.plot(xNp[i, np.where(xNp[i] != 0)], yNp[i, np.where(yNp[i] != 0)], 'b.')
+                    else:
+                        plt.plot(xNp[i, :], yNp[i, :], 'b--', linewidth=1, label=labels[i])
                 else:
                     plt.plot(xNp[i, :], yNp[i, :], label=labels[i])
+
+            if type == 2:
+                plt.plot(xNp[1, 0], yNp[1, 0], 'r.', label=labels[1])
+                plt.plot(xNp[2, 0], yNp[2, 0], 'b.', label=labels[2])
         else:
             for i in range(1):
                 plt.plot(xNp[i, :], yNp[i, :], label=mem.title())
 
     xStart = np.array([xNp[0, 0]])
     yStart = np.array([yNp[0, 0]])
+
+    xEnd, yEnd = getEnding(type)
+
+    plt.title(f"Comparasion of paths of vehicles [Route {type}]")
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.plot(xStart, yStart, 'c*', label="Start")
+    plt.plot(xEnd, yEnd, 'y*', label="End")
+    plt.legend()
+    plt.savefig(f'results/compare{type}.png')
+
+
+def getEnding(type):
     if type == 0:
         xEnd = np.array([-490])
         yEnd = np.array([174])
@@ -146,13 +173,8 @@ def plotPathAllInOne(type):
     elif type == 2:
         xEnd = np.array([220.5])
         yEnd = np.array([-169.5])
-    plt.title(f"Comparasion of paths of vehicles")
-    plt.xlabel("x")
-    plt.ylabel("y")
-    plt.plot(xStart, yStart, 'c*', label="Start")
-    plt.plot(xEnd, yEnd, 'y*', label="End")
-    plt.legend()
-    plt.savefig(f'results/compare{type}.png')
+
+    return xEnd, yEnd
 
 
 def actionForAllResults():
@@ -169,8 +191,8 @@ if __name__ == '__main__':
     # plotPath(res)
     #
     #createGeneticForAllInOne()
-    #plotPathAllInOne(0)
-    #plotPathAllInOne(1)
-    plotPathAllInOne(2)
-    #actionForAllResults()
+    # plotPathAllInOne(0)
+    # plotPathAllInOne(1)
+    # plotPathAllInOne(2)
+    actionForAllResults()
     plt.show()
